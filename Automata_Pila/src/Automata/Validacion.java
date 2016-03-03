8b760e50eaf04ba5b9e3b4d8f3edb8f3f57a6f36
jose0410 <jose11fernando@hotmail.com>
2016-03-03 15:05:08 -0500

    prueba

---
+++ Automata_Pila/src/Automata/Validacion.java
@@ -0,0 +1,24 @@
+package Automata;
+import java.util.Stack;
+/**
+ *
+ * @author Nando
+ */
+public class Validacion {
+    
+    
+    public static void validar(String h){        
+        char[] texto = h.toCharArray();
+        char[] simbolosTabla = new char[texto.length - 1];
+        for(int i = 0; i < texto.length; i++){
+            if(texto[0]==','){
+                
+            }else{
+                if(texto.length == 1){
+                    
+                }
+            }            
+        }
+    }
+    
+}
