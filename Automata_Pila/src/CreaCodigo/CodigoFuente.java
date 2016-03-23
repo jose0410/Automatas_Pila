c4849c166b100766e1a5ddfeb706727185cd68f0
PedroGallego <PedroGallego@localhost>
2016-03-22 22:12:40 -0500

    agregue la forma de crear el archivo.java y cree la clase en la que se resuelve el automata

--- Automata_Pila/src/CreaCodigo/CodigoFuente.java
+++ Automata_Pila/src/CreaCodigo/CodigoFuente.java
@@ -5,10 +5,34 @@
  */
 package CreaCodigo;
 
+import Automata.Automata;
+import java.io.File;
+import java.io.FileWriter;
+
 /**
  *
  * @author PedroGallego
  */
 public class CodigoFuente {
+    private Automata automata;
     
+    public CodigoFuente (Automata automata) {
+        this.automata = automata;
+    }
+    
+    public boolean crearArchivoJava (String codigo,String ruta,String nombre) {
+        try {
+            File archivo = new File(ruta+nombre+".java");
+            FileWriter escribir = new FileWriter(archivo,true);
+            escribir.write(codigo);
+            escribir.close();
+            return true;
+        }catch (Exception e){
+            return false;
+        }
+    }
+    
+    public String crearCodigoJava () {
+        return "";
+    }
 }
